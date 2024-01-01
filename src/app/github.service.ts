37a31dfe9d2c7b5d08ac7b349a50d40f4900d421
fifoll <kakolfilip@wp.pl>
2024-01-01 14:34:13 +0100

    add repositories and panel component, add repository interface

--- src/app/github.service.ts
+++ src/app/github.service.ts
@@ -2,18 +2,19 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { User } from './user';
+import { Repository } from './repository';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GithubService {
-
   readonly username = 'fifoll';
-  private apiUrl = 'https://api.github.com';
-
-  constructor(private http: HttpClient) { };
-
+  private userUrl = 'https://api.github.com/users/' + this.username;
+  constructor(private http: HttpClient) { }
   getUser(): Observable<User> {
-    return this.http.get<User>(`${this.apiUrl}/users/${this.username}`);
+    return this.http.get<User>(this.userUrl);
+  }
+  getRepos(): Observable<Repository[]> {
+    return this.http.get<Repository[]>(this.userUrl + '/repos');
   }
 }
